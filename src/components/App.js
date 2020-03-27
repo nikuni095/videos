0da7e5c412e3b7912c9a1ff3cad6305c3a97dcac
nikuni095 <nikhil.uni095@gmail.com>
2020-03-27 13:56:08 +0530

    first coomit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+
+
+class App extends React.Component{
+    render(){
+        return(
+            <div>hi</div>
+        )
+    }
+}
+export default App;
